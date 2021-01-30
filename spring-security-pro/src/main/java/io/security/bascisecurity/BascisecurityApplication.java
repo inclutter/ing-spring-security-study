95ebfb4a57f4973009f8ff913325fe5122f522f3
mwkang <augustman8808@gmail.com>
2021-01-30 18:50:16 +0900

    Project initialized

---
+++ spring-security-pro/src/main/java/io/security/bascisecurity/BascisecurityApplication.java
@@ -0,0 +1,13 @@
+package io.security.bascisecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BascisecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BascisecurityApplication.class, args);
+    }
+
+}
---
+++ spring-security-pro/src/test/java/io/security/bascisecurity/BascisecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.bascisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BascisecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
