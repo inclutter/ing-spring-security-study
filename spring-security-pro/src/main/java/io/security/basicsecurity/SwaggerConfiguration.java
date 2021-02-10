fba3c199f971de21371cf3fb2b6baaac1e9b9963
mwkang <augustman@knou.ac.kr>
2021-02-10 17:03:31 +0900

    [ADD] swagger list library have been added.

---
+++ spring-security-pro/src/main/java/io/security/basicsecurity/SwaggerConfiguration.java
@@ -0,0 +1,31 @@
+package io.security.basicsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket docket() {
+
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+        apiInfoBuilder.title("API 서버 문서");
+        apiInfoBuilder.description("API 서버 문서입니다.");
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+        docket.apiInfo(apiInfoBuilder.build());
+
+        ApiSelectorBuilder apiSelectorBuilder = docket.select().apis(RequestHandlerSelectors.basePackage("io.security.basicsecurity"));
+        apiSelectorBuilder.paths(PathSelectors.ant("/**"));
+        return apiSelectorBuilder.build();
+    }
+}
